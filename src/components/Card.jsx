bc3a98998fa7bc4e444480b48095c504bb1ffac5
shahmansoor1111 <shahmansoor0316@gmail.com>
2025-09-17 10:26:02 +0500

    ok

---
+++ src/components/Card.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import "./Card.css"; // Import CSS
+
+const Card = () => {
+  const messages = [
+    "Tell something About your Teacher",
+    "Our Teacher Name is Sir Tatheer",
+    "HardWorking",
+    "Handsome",
+    "Kam age mai Mashallah buhut Ameer hai",
+    "Punctuality",
+  ];
+
+  const [index, setIndex] = useState(0);
+  const [badHabit, setBadHabit] = useState("");
+
+  const handleClick = () => {
+    setBadHabit("");
+    setIndex((prevIndex) => (prevIndex + 1) % messages.length);
+  };
+
+  const Another = () => {
+    setBadHabit("Mai nahi bataonga ");
+  };
+
+  return (
+    <div className="card-container">
+      <h1 className="card-text">{badHabit ? badHabit : messages[index]}</h1>
+
+      <div className="btn-group">
+        <button className="btn good-btn" onClick={handleClick}>
+          Good Habits
+        </button>
+        <button className="btn bad-btn" onClick={Another}>
+          Bad Habits
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
